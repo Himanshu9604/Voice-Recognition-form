fffc7051ee1c518c1d07eaa7c12c77b6227747ff
Himanshu <himanshuyadav201000@gmail.com>
2023-10-18 10:52:05 +0530

    done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,7 @@ import "./Form.css";
 import { BsFillMicFill } from "react-icons/bs";
 
 function Form() {
+  // State to store form data
   const [data, setData] = useState({
     FirstName: "",
     LastName: "",
@@ -13,17 +14,20 @@ function Form() {
     AadhaarNumber: "",
   });
 
+  // Check if speech recognition is supported
   if ("webkitSpeechRecognition" in window) {
     console.log("Congratulations");
   } else {
     console.log("Speech recognition is not supported in this browser.");
   }
 
+  // Function to handle voice input
   const handleVoiceInput = (inputField) => {
     const recognition = new window.webkitSpeechRecognition();
     recognition.continuous = false;
     recognition.lang = "en-US";
 
+    // Handle voice recognition results
     recognition.onresult = (event) => {
       const transcript = event.results[0][0].transcript;
       inputField.value = transcript;
@@ -32,32 +36,39 @@ function Form() {
     recognition.start();
   };
 
+  // Function to handle form input changes
   const handleInput = (e) => {
     const { name, value } = e.target;
     setData({ ...data, [name]: value });
   };
 
+  // Function to handle form submission
   function handleSubmit(e) {
     e.preventDefault();
     const isFormValid = validateForm(data);
 
     if (isFormValid) {
+      // Get existing data from local storage or create an empty array
       let existingData = JSON.parse(localStorage.getItem("formData")) || [];
 
       if (!Array.isArray(existingData)) {
         existingData = [];
       }
 
+      // Add the current form data to the existing data
       existingData.push(data);
 
+      // Store the updated data in local storage
       localStorage.setItem("formData", JSON.stringify(existingData));
       console.log("Data submitted:", data);
     }
   }
 
+  // Function to validate form data
   const validateForm = (formData) => {
     let isValid = true;
 
+    // Regular expressions for validation
     const namePattern = /^[A-Za-z\s]+$/;
     const statePattern = /^[A-Za-z\s]+$/;
     const districtPattern = /^[A-Za-z\s]+$/;
@@ -65,47 +76,12 @@ function Form() {
     const panPattern = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
     const aadhaarPattern = /^[0-9]{12}$/;
 
+    // Validation checks
     if (!namePattern.test(formData.FirstName)) {
       isValid = false;
       alert(
         "First Name is not valid. It should contain only letters and spaces."
       );
-    }
-
-    if (!namePattern.test(formData.LastName)) {
-      isValid = false;
-      alert(
-        "Last Name is not valid. It should contain only letters and spaces."
-      );
-    }
-
-    if (!statePattern.test(formData.State)) {
-      isValid = false;
-      alert("State is not valid. It should contain only letters and spaces.");
-    }
-
-    if (!districtPattern.test(formData.District)) {
-      isValid = false;
-      alert(
-        "District is not valid. It should contain only letters and spaces."
-      );
-    }
-
-    if (!villagePattern.test(formData.Village)) {
-      isValid = false;
-      alert("Village is not valid. It should contain only letters and spaces.");
-    }
-
-    if (!panPattern.test(formData.PANNumber)) {
-      isValid = false;
-      alert(
-        "PAN Number is not valid. It should match the pattern: AAAAA9999A."
-      );
-    }
-
-    if (!aadhaarPattern.test(formData.AadhaarNumber)) {
-      isValid = false;
-      alert("Aadhaar Number is not valid. It should be a 12-digit number.");
     }
 
     return isValid;
